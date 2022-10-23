7a8e617642619b98b377b2fcb27917e175549b20
snitch101 <93768724+snitch101@users.noreply.github.com>
2022-10-23 14:35:45 +0200

    upload

---
+++ 12.sql
@@ -0,0 +1,1 @@
+SELECT movies.title FROM movies JOIN stars ON stars.movie_id = movies.id WHERE movies.id IN (SELECT stars.movie_id FROM stars JOIN people ON stars.person_id = people.id WHERE people.name = "Helena Bonham Carter" AND stars.movie_id IN (SELECT stars.movie_id FROM stars JOIN people ON stars.person_id = people.id WHERE people.name = "Johnny Depp"))
---
+++ 13.sql
@@ -0,0 +1,1 @@
+SELECT people.name FROM people JOIN stars ON stars.person_id = people.id WHERE stars.movie_id IN (SELECT stars.movie_id FROM stars JOIN people ON stars.person_id = people.id WHERE people.name = "Kevin Bacon" AND people.birth = 1958) AND people.name != "Kevin Bacon";
